4efae801c04af82e599a39c1b5d52072d4047747
Almas <eleron888@gmail.com>
2020-04-09 16:05:14 +0200

    count of people

--- covid.go
+++ covid.go
@@ -2,31 +2,51 @@ package covid
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 type Full struct {
-	Code int
-	Data []Data
+	Code int    `json:"code"`
+	Data []Data `json:"data"`
 }
 
 type Data struct {
-	Location    string
-	CountryCode string
-	Latitude    float64
-	Longtitude  float64
-	Confirmed   int
-	Dead        int
-	Recovered   int
-	Updated     time.Time
+	Location    string  `json:"location"`
+	CountryCode string  `json:"country_code"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	Confirmed   int     `json:"confirmed"`
+	Dead        int     `json:"dead"`
+	Recovered   int     `json:"recovered"`
+	Updated     string  `json:"updated"`
 }
 
-func Info(info *Full) error {
+func Info(countryName string) int {
 	resp, err := http.Get("https://www.trackcorona.live/api/countries")
 	if err != nil {
-		return err
+		panic(err)
 	}
 	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(info)
+	var info Full
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &info)
+	if err != nil {
+		panic(err)
+	}
+	i := 0
+	var confirmed int
+	for _, v := range info.Data {
+		if countryName == v.Location {
+			confirmed = v.Confirmed
+			i++
+		}
+	}
+	if i == 0 {
+		return -1
+	}
+	return confirmed
 }
